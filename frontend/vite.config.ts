964ed42c29fd23bbc45bb2dc1751706148ed9546
Olatunbosun-Oluwatosin <tolalegbin@gmail.com>
2024-07-08 00:17:14 +0100

    updated vite-configuration file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,8 @@ import { defineConfig } from "vite"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
+  server: {
+    host: '0.0.0.0',  // This allows the server to be accessible externally
+    port: 5173,       // Ensure this port matches your Docker run command
+  }
 })
